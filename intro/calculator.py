4023bac69939ec3b345af3519c13970ba726f5bd
griff182uk <griff182uk@yahoo.co.uk>
2024-12-10 08:03:52 +0000

    docs: :memo: done intro module

---
+++ intro/calculator.py
@@ -0,0 +1,3 @@
+def calculate_average(numbers):
+    return sum(numbers) / len(numbers) # copilot tab completed this
+
---
+++ intro/comment.py
@@ -0,0 +1,3 @@
+# Description: This is a simple function that takes a string and returns the reverse of it.
+def reverse_string(s): # I only wrote this
+    return s[::-1]
